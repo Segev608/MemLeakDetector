fdf9b79a829b4dc1b33818669803ed7f447fbeb8
Segev608 <57367786+Segev608@users.noreply.github.com>
2021-06-18 14:17:28 +0300

    Update free-twice.c

--- Memcheck/test/free-twice/free-twice.c
+++ Memcheck/test/free-twice/free-twice.c
@@ -3,6 +3,6 @@
 int main(){
 	int* p = malloc(60);
 	free(p); // <-- good practice
-	free(p); // <-- bad!, freed null pointer. Invalid operation!
+	free(p); // <-- bad!, freed pointer twice. Invalid operation!
 	return 0;
 }
