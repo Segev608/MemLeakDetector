7aa4a2832584d5fc00d79d085f0cf3a94630859f
Segev608 <57367786+Segev608@users.noreply.github.com>
2021-06-24 11:44:04 +0300

    Add files via upload

---
+++ Memcheck/test/external-leak-by-dll/dllmain.cpp
@@ -0,0 +1,26 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    void* p = NULL;
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        printf("Entered dll\n");
+        p = malloc(70);
+        break;
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        printf("Exiting dll\n");
+        //free(p);   <--- problem
+        printf("LEAK\n");
+        break;
+    }
+    return TRUE;
+}
+
